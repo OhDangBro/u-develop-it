15722d1ce0401e00b58bd5cc8bf90f59828ba4c2
Will <103394466+OhDangBro@users.noreply.github.com>
2022-07-22 19:04:46 -0400

    added api const sql commands

--- server.js
+++ server.js
@@ -32,7 +32,12 @@ app.get('/', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?`;
     const params = [req.params.id];
   
     db.query(sql, params, (err, row) => {
@@ -106,7 +111,11 @@ db.query(sql, params, (err, result) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id`;
   
     db.query(sql, (err, rows) => {
       if (err) {
